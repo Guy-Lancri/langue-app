ac034f472a81d734e6e01525d58e09c7d7700e32
Guy-Lancri <guy.lancri@gmail.com>
2026-04-15 10:39:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def home():
     return render_template("index.html", mot=mot, message=message)
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+   app.run(debug=False, use_reloader=False)
